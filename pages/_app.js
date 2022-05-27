67dd8760e043944d47a3fe79a8ba34e744a6b475
Hoan Tran <hoan.tranthi@duluxgroup.com.au>
2022-05-27 14:21:16 +0700

    update app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>web app</title>
+      </Head>
+      <div>This is navbar</div>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
